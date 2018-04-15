84efe2066059aad81d90ada282d26ac7fad413b4
redtoorange <redtoorange@gmail.com>
2018-04-15 17:52:38 -0400

    Improved Security of code and added score filters.

--- admin/admin.php
+++ admin/admin.php
@@ -31,7 +31,7 @@
 <body class="fixed-nav bg-dark" id="page-top">
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand mr-auto" href="index.html">Data Administration</a>
+    <a class="navbar-brand mr-auto" href="../index.html">Data Administration</a>
 	<?php			
 		if( isset($_SESSION['username']) ){
 			echo '
@@ -48,33 +48,73 @@
 
   
   
-  <div class="content-wrapper">
+<div class="content-wrapper">
   
 	<!-- Data Controls Bit -->
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col">
-				<h3><i class="fa fa-download"></i> Data Controls</h3>
+			<div class="row">
+					<div class="col">
+							<h3><i class="fa fa-download"></i> Data Controls</h3>
+					</div>
 			</div>
-		</div>
 		
-		<div class="row">
-			<div class="col">
-				<a class="btn btn-secondary" href="/admin/testFile.txt" role="button"  download> Download File </a>
-				
-				<?php
-					if( isset($_SESSION['username']) ){
-						echo '<button id="themeButton" type="button" class="btn btn-secondary">Change Themes</button>'."\n";
-						echo '<button id="deleteButton" type="button" class="btn btn-danger">Delete Data</button>';
-					}
-				?>
-				
+			<div class="row">
+					<div class="col">
+						<a class="btn btn-secondary" href="/admin/dataFile.txt" role="button"  download> Download File </a>
+						<?php
+							if( isset($_SESSION['username']) ){
+								echo '<button id="themeButton" type="button" class="btn btn-secondary">Change Themes</button>'."\n";
+								echo '<button id="deleteButton" type="button" class="btn btn-danger">Delete Data</button>';
+							}
+						?>
+					</div>
+			</div> 
+		
+		<br/>
+
+			<div class="row">
+				<div class="col">
+				<form method="post" action="admin.php" class="form-inline">
+					<div class="form-group mr-2">
+						<select name="inputSelect" id="inputSelect" class="form-control">
+							<option value="">Input Device</option>
+							<option value="mouse">Mouse</option>
+							<option value="touch">Touch Screen</option>
+							<option value="trackPad">Track Pad</option>
+						</select>
+					</div>
+
+					<div class="form-group mr-2">
+						<select name="ageSelect" id="ageSelect" class="form-control">
+								<option value="">Age Group</option>
+								<option value="age1">13-18</option>
+								<option value="age2">19-27</option>
+								<option value="age3">28-35</option>
+								<option value="age4">36-45</option>
+							</select>
+					</div>
+
+					<div class="form-group mr-2">
+						<select name="skillSelect" id="skillSelect" class="form-control">
+							<option value="">Skill Level</option>
+							<option value="beginner">Beginner</option>
+							<option value="intermediate">Intermediate</option>
+							<option value="experienced">Experienced</option>
+							<option value="advanced">Advanced</option>
+						</select>
+					</div>
+
+					<button id="filterSubmit" type="submit" class="btn btn-secondary mr-2">Apply Filter</button>
+					<button id="filterClear" type="submit" class="btn btn-danger">Clear Filters</button>
+					</form>
+				</div>
 			</div>
-		</div> 
 	</div>
+
+	
 	<!-- End Data Controls -->
   
-	</br>
+	<br/>
 
 	<!-- Data Table Bit -->
     <div class="container-fluid">
@@ -98,63 +138,98 @@
 				  <th>Targets Hit</th>
                 </tr>
               </thead>
-              <tfoot>
+              
+              <tbody>
+				<?php
+					require "../db_connect/createConnection.php";
+					
+					// Create the connection to the DB
+					$conn = ConnectToDB();
+
+					// Initial Query to the DB
+					$query = 'SELECT * FROM  `test_data` ';
+
+					// Check for post data from the filters
+					$inputFilter = isset( $_POST["inputSelect"]) && $_POST["inputSelect"] != "";
+					$ageFilter = isset( $_POST["ageSelect"]) && $_POST["ageSelect"] != "";
+					$skillFilter = isset( $_POST["skillSelect"]) && $_POST["skillSelect"] != "";
+
+					// If there are any filters, then add the elements to the query
+					if( $inputFilter || $ageFilter || $skillFilter ){
+						$query .= "WHERE ";
+
+						if( $inputFilter ){
+							$query .= "`input` = '".$_POST["inputSelect"]."' ";
+
+							if( $ageFilter || $skillFilter)
+								$query .= " AND ";
+						}
+
+						if( $ageFilter ){
+							$query .= "`age` = '".$_POST["ageSelect"]."' ";
+
+							if( $skillFilter)
+								$query .= " AND ";
+						}
+	
+						if( $skillFilter ){
+							$query .= "`skill` = '".$_POST["skillSelect"]."' ";
+						}
+					}
+
+					// Debug Query
+					//echo $query;
+
+					// Query the DB
+					$result = $conn->query($query);
+					//$numFields = $result->field_count;
+					
+					// Create and open the file for the data
+					$myFile = "dataFile.txt";
+					$fo = fopen($myFile, 'w') or die("can't open file");
+
+					// Create an Array of the col names from the DB
+					$columns = array("ID", "input", "age", "skill", "score", "Targets");
+					
+					// Create an Array to hold all the rows from the DB
+					$rows = array();
+
+					// Create the table rows from the DB Data
+					while($row = $result->fetch_assoc()){
+						$rows[] = $row;	// Add the row to rows
+
+						// Print the table to HTML
+						echo '<tr>';
+						for ($i = 0; $i < sizeof($columns); $i++){
+							echo '<td>';
+							echo $row[$columns[$i]];
+							echo '</td>';
+						}
+						echo '</tr>';
+
+					}
+					
+					// Write the rows to the dataFile
+					fwrite($fo, json_encode( $rows, JSON_PRETTY_PRINT ) );
+					fclose($fo);
+				?>
+
+							</tbody>
+
+							<tfoot>
                 <tr>
                   <th>ID</th>
                   <th>Input</th>
                   <th>Age</th>
                   <th>Skill</th>
                   <th>Score</th>
-				  <th>Targets Hit</th>
+				  				<th>Targets Hit</th>
                 </tr>
-              </tfoot>
-              <tbody>
-				<?php
-					require "../db_connect/createConnection.php";
-					
-					//function to create a table
-					$conn = ConnectToDB();
-
-					// Query Data from the DB
-					$query = 'SELECT * FROM  `test_data` ';
-					$result = $conn->query($query);
-					$numFields = $result->field_count;
-					
-					$myFile = "testFile.txt";
-					$fo = fopen($myFile, 'w') or die("can't open file");
-
-					// Create an Array of the col names from the DB
-					$columns = array("ID", "input", "age", "skill", "score", "Targets");
-					
-					$rows = array();
-					// Create the table rows from the DB Data
-					while($row = $result->fetch_assoc()){
-						$rows[] = $row;
-						echo '<tr>';
-						
-						// For each Col in the DB data, create a col in the table
-						for ($i = 0; $i < sizeof($columns); $i++){
-							// Print the DB Data into the table data
-							echo '<td>';
-							echo $row[$columns[$i]];
-							echo '</td>';
-						}
-						
-						echo '</tr>';
-					}
-					
-					fwrite($fo, json_encode( $rows, JSON_PRETTY_PRINT ) );
-					fclose($fo);
-				?>
-              </tbody>
+							</tfoot>
+							
             </table>
           </div>
         </div>
-		
-		
-        <div class="card-footer small text-muted">
-			Updated yesterday at 11:59 PM
-		</div>
       </div>
     </div>
 	<!-- End Data Table -->
@@ -186,7 +261,7 @@
 			  
 			  <div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal" >Cancel</button>
-				<button id="loginSubmit" type="sumit" class="btn btn-primary">Login</button>
+				<button id="loginSubmit" type="submit" class="btn btn-primary">Login</button>
 			  </div>
 		  </form>
 		  
@@ -266,18 +341,25 @@
 		</div>
 	  </div>
 	</div>
-	
+
+</div>
 	
     <!-- Bootstrap Core -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.js"></script>
 	
-	<script type="text/javascript">
-		// Load the table into jQueryDataTables
+	<script>
+		// Executed when the document is ready
 		$(document).ready(function() {
+			// Initialize the data table
 		  $('#dataTable').DataTable();
+
+			// Set all the filters to the currently displayed values
+			$("#inputSelect").val("<?php if(isset($_POST["inputSelect"]) ){echo $_POST["inputSelect"];} ?>");
+			$("#ageSelect").val("<?php if(isset($_POST["ageSelect"]) ){echo $_POST["ageSelect"];} ?>");
+			$("#skillSelect").val("<?php if(isset($_POST["skillSelect"]) ){echo $_POST["skillSelect"];} ?>");
 		});
 		
 		// Display Login Panel
@@ -294,80 +376,100 @@
 		$('#themeButton').click(function(){
 			$('#themeModal').modal('show');
 		});
-	</script>
+
+		// When the filter clear button is clicked, reset all selects
+		$("#filterClear").click( function(){
+				$("#inputSelect").val("");
+				$("#ageSelect").val("");
+				$("#skillSelect").val("");
+		});
+
+		// Handle login ajax call
+		function admin_login() {
+			var user = $("#adminUsername").val();
+			var pass  = $("#adminPassword").val();
+			
+			if(user != "" && pass != "") {
+				$.ajax({
+					type:'post',
+					url:'../db_connect/databaseController.php',
+					data:{ whatToDo: "adminLogin", do_login: "do_login", username: user, password: pass },
+					
+					success:	function(response) {
+									if(response=="success") {
+										window.location.href="admin.php";
+									}
+									else {
+										alert("Invalid Login Credentials.");
+									}
+								}
+				});
+				
+			}
+			else {
+				alert("Please Fill All The Details");
+			}
+
+			return false;
+		}
 	
-<script type="text/javascript">
-	function admin_login() {
-		var user = $("#adminUsername").val();
-		var pass  = $("#adminPassword").val();
-		
-		if(user != "" && pass != "") {
+		// Handle delete data ajax (Hidden is not logged in)
+		<?php
+			if( isset($_SESSION["username"]) ){
+				echo '
+		function delete_data() {
 			$.ajax({
-				type:'post',
-				url:'../db_connect/databaseController.php',
-				data:{ whatToDo: "adminLogin", do_login: "do_login", username: user, password: pass },
+				type:"post",
+				url:"../db_connect/databaseController.php",
+				data:{ whatToDo: "deleteData" },
 				
 				success:	function(response) {
 								if(response=="success") {
 									window.location.href="admin.php";
 								}
-								else {
-									alert("Invalid Login Credentials.");
-								}
 							}
 			});
 			
+			return false;			
 		}
-		else {
-			alert("Please Fill All The Details");
-		}
+				';
 
-		return false;
-	}
+			}
+		?>
+		
 	
-	
-	function delete_data() {
-		$.ajax({
-			type:'post',
-			url:'../db_connect/databaseController.php',
-			data:{ whatToDo: "deleteData" },
+		// handle change theme ajax (Hidden is not logged in)
+
+		<?php
+			if( isset($_SESSION["username"]) ){
+				echo '
+		function change_theme() {
+			var selectedTheme = "both";
 			
-			success:	function(response) {
-							if(response=="success") {
-								window.location.href="admin.php";
+			if( $("#carnivalTheme").is(":checked") ){
+				selectedTheme = "carnival";
+			}
+			if( $("#spaceTheme").is(":checked") ){
+				selectedTheme = "space";
+			}
+			
+			$.ajax({
+				type: "post",
+				url:"../db_connect/databaseController.php",
+				data:{ whatToDo: "setTheme", theme: selectedTheme },
+				
+				success:	function(response) {
+								$("#themeModal").modal("hide");
 							}
-						}
-		});
-		
-		return false;			
-	}
-	
-	function change_theme() {
-		var selectedTheme = "both";
-		
-		if( $("#carnivalTheme").is(":checked") ){
-			selectedTheme = "carnival";
-		}
-		if( $("#spaceTheme").is(":checked") ){
-			selectedTheme = "space";
-		}
-		
-		$.ajax({
-			type:'post',
-			url:'../db_connect/databaseController.php',
-			data:{ whatToDo: "setTheme", theme: selectedTheme },
+			});
 			
-			success:	function(response) {
-							$('#themeModal').modal('hide');
-						}
-		});
-		
-		return false;			
-	}
-
+			return false;			
+				}
+				';
+			}
+		?>
 		
 </script>
-	
-  </div>
+
 </body>
 </html>
